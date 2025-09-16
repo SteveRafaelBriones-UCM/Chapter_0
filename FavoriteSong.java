f713edeec6f874c84c4de52ed5340a4eebfaaf6c
SteveRafaelBriones-UCM <steverafaelb@gmail.com>
2025-09-16 09:04:23 +0800

    Add files via upload

---
+++ FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong{
+   public static void main(String[] args){
+   System.out.println("Kung tayo ay matanda na Sana'y di tayo magbago");
+   System.out.println("Makuha mo pa kayang Ako'y hagkan at yakapin oh");
+   System.out.println("Kung maputi na ang buhok ko");
+   System.out.println("Pagdating ng araw Ang 'yong buhok ay puputi na rin");
+   }
+}
